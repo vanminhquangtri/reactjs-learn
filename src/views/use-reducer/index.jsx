a1400ab875db577bb56c358a7e414ba92e54035b
Van Minh <vanminhquangtri@gmail.com>
2022-03-29 09:57:52 +0700

    use reducer

--- src/views/use-reducer/index.jsx
+++ src/views/use-reducer/index.jsx
@@ -1,32 +1,42 @@
-import React, { useReducer } from 'react';
-
-const initCount = 0;
-
-const DOWN_ACTION = 'down';
-const UP_ACTION = 'up';
-
-const countReducer = (state, action) => {
-  switch (action) {
-    case DOWN_ACTION:
-      return state - 1;
-    case UP_ACTION:
-      return state + 1;
-    default:
-      throw new Error('Invalid action');
-  }
-};
+import React, { useReducer, useRef } from 'react';
+import { addJob, deleteJob, initState, reducer, setJob } from './reducer';
 
 const TestUseReducer = () => {
-  const [count, dispatch] = useReducer(countReducer, initCount);
+  const [state, dispatch] = useReducer(reducer, initState);
+  const jobRef = useRef();
+  const { job, jobs } = state;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addJob(job));
+    dispatch(setJob(''));
+    jobRef.current.focus();
+  };
+
   return (
     <div>
       <strong>Use Reducer</strong>
       <div>
-        Count: <strong>{count}</strong>
+        <form onSubmit={handleSubmit}>
+          <input
+            ref={jobRef}
+            type="text"
+            value={job}
+            onChange={(e) => {
+              dispatch(setJob(e.target.value));
+            }}
+          />
+          <button type="submit">Add</button>
+        </form>
       </div>
       <div>
-        <button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
-        <button onClick={() => dispatch(UP_ACTION)}>Up</button>
+        {jobs.map((j, index) => {
+          return (
+            <div key={index}>
+              {j} <button onClick={() => dispatch(deleteJob(index))}>x</button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/views/use-reducer/reducer.js
+++ src/views/use-reducer/reducer.js
@@ -6,3 +6,48 @@ export const initState = {
 export const SET_JOB = 'SET_JOB';
 export const ADD_JOB = 'ADD_JOB';
 export const DELETE_JOB = 'DELETE_JOB';
+
+export const setJob = (job) => {
+  return {
+    type: SET_JOB,
+    payload: job,
+  };
+};
+
+export const addJob = (job) => {
+  return {
+    type: ADD_JOB,
+    payload: job,
+  };
+};
+
+export const deleteJob = (jobIndex) => {
+  return {
+    type: DELETE_JOB,
+    payload: jobIndex,
+  };
+};
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_JOB:
+      return {
+        ...state,
+        job: action.payload,
+      };
+    case ADD_JOB:
+      return {
+        ...state,
+        jobs: [...state.jobs, action.payload],
+      };
+    case DELETE_JOB:
+      return {
+        ...state,
+        jobs: state.jobs.filter((_, index) => {
+          return index !== action.payload;
+        }),
+      };
+    default:
+      return state;
+  }
+};
